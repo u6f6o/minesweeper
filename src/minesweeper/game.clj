3ace5996fd4cc5830381564575384c41d14d6f90
Ulf Gitschthaler <ulf@gitschthaler.de>
2015-05-03 20:13:15 +0200

    place mines and warnings on board

--- src/minesweeper/game.clj
+++ src/minesweeper/game.clj
@@ -7,41 +7,49 @@
   [w h]
   (vec (repeat w (vec (repeat h nil)))))
 
-(defn populate
-  "Places a mine on each of the cells specified as [x, y] coordinates"
-  [board mined-cells]
-  (reduce (fn [board coordinates]
-            (assoc-in board coordinates {:mine true}))
-          board
-          mined-cells))
-
-(defn locate-mines
-  "Locate mines on the board"
-  [board]
-  (for [[x row] (map-indexed vector board)
-        [y val] (map-indexed vector row)
-        :when (:mine val)]
-    [x y]))
-
 (defn neighbours
   "Locate a given cells' neighbour cells"
   [[x y]]
   (for [dx [-1 0 1 ] dy [-1 0 1] :when (not= 0 dx dy)]
     [(+ dx x) (+ dy y)]))
 
+(defn locate-mines
+  "Locate mines coordinates on the board"
+  [board]
+  (for [[x row] (map-indexed vector board)
+        [y val] (map-indexed vector row)
+        :when (:mine val)]
+    [x y]))
+
+(defn locate-warnings
+  "Locate warnings coordinates on the board"
+  [board]
+  (let [mines (locate-mines board)]
+    (remove #(get-in board %) (mapcat neighbours mines))))
+
+(defn place-mines
+  "Place mines on the board"
+  [board mines]
+  (reduce
+    (fn [board coordinates]
+      (assoc-in board coordinates {:mine true}))
+   board
+   mines))
+
 (defn place-warnings
   [board]
-  (let [mines (locate-mines board)
-        warnings (mapcat neighbours mines)]
+  (let [warnings (locate-warnings board)]
     (reduce
-     (fn [board coordinates]
-       (update-in board (conj coordinates :warn) (fnil inc 0)))
+      (fn [board coordinates]
+        (update-in board (conj coordinates :warn) (fnil inc 0)))
      board
      warnings)))
 
+(defn populate
+  "Places a mine on each of the cells specified as [x, y] coordinates"
+  [board mined-cells]
+  (place-warnings (place-mines board mined-cells)))
 
-(def bar [{:mine 4}])
-(update-in bar [0 :mine] (fnil inc 0))
 
 (def glider (populate (empty-board 10 10) #{[1 1] [1 2]}))
-(pprint (place-warnings glider))
+(pprint  glider)
