005cff6484b3881820bcd95fdaed07b9cb9740fc
Ulf Gitschthaler <ulf@gitschthaler.de>
2015-05-04 11:36:30 +0200

    restructure stuff

--- src/minesweeper/core.clj
+++ src/minesweeper/core.clj
@@ -1,5 +1,10 @@
 (ns minesweeper.core
+  (:use [minesweeper.game]
+        [clojure.pprint])
   (:gen-class))
+
+
+(init-game 10 10 25)
 
 (defn -main
   "I don't do a whole lot ... yet."
--- src/minesweeper/game.clj
+++ src/minesweeper/game.clj
@@ -1,55 +1,71 @@
 (ns minesweeper.game
   (:use [clojure.pprint]))
 
-
-(defn empty-board
-  "Creates a rectangular empty board of the specified with and height"
+(defn- empty-board
+  "Create a rectangular empty board of the specified with and height"
   [w h]
   (vec (repeat w (vec (repeat h nil)))))
 
-(defn neighbours
-  "Locate a given cells' neighbour cells"
-  [[x y]]
-  (for [dx [-1 0 1 ] dy [-1 0 1] :when (not= 0 dx dy)]
-    [(+ dx x) (+ dy y)]))
+(defn to-coords
+  ([board]
+   (to-coords board (constantly true)))
+  ([board pred]
+   (let [w (count board)
+         h (count (first board))]
+     (for [x (range w) y (range h) :when (pred board x y)]
+       [x y]))))
 
-(defn locate-mines
-  "Locate mines coordinates on the board"
+(defn board->coords
+  "Coordinates of the board"
   [board]
-  (for [[x row] (map-indexed vector board)
-        [y val] (map-indexed vector row)
-        :when (:mine val)]
-    [x y]))
+  (to-coords board))
 
-(defn locate-warnings
-  "Locate warnings coordinates on the board"
+(defn mines->coords
+  "Coordinates of the mines on the board"
   [board]
-  (let [mines (locate-mines board)]
-    (remove #(get-in board %) (mapcat neighbours mines))))
+  (to-coords board #(get-in %1 [%2 %3 :mine])))
 
-(defn place-mines
-  "Place mines on the board"
-  [board mines]
-  (reduce
-    (fn [board coordinates]
-      (assoc-in board coordinates {:mine true}))
-   board
-   mines))
-
-(defn place-warnings
-  [board]
-  (let [warnings (locate-warnings board)]
+(defn- place-mines
+  "Place n mines randomly on the board"
+  [board n]
+  (let [mines (take n (shuffle (to-coords board)))]
     (reduce
-      (fn [board coordinates]
-        (update-in board (conj coordinates :warn) (fnil inc 0)))
+     (fn [board coordinates]
+       (assoc-in board coordinates {:mine true}))
      board
-     warnings)))
+     mines)))
 
-(defn populate
-  "Places a mine on each of the cells specified as [x, y] coordinates"
-  [board mined-cells]
-  (place-warnings (place-mines board mined-cells)))
+(defn neighbours
+  "Locate neighbour cells based on coordinates [x y],
+  respecting board width and height"
+  [board [x y]]
+  (let [w (count board)
+        h (count (first board))]
+    (for [dx (map #(+ % x) [-1 0 1])
+          dy (map #(+ % y) [-1 0 1])
+          :when (and (or (not= x dx) (not= y dy))
+                     (> w dx -1)
+                     (> h dy -1))]
+      [dx dy])))
+
+(defn- place-warnings
+  "Place warnings on a mines' neighbour cells"
+  [board]
+  (let [ws (->> board
+                mines->coords
+                (mapcat (partial neighbours board))
+                (remove #(get-in board (conj % :mine))))]
+    (reduce
+     (fn [board coordinates]
+       (update-in board (conj coordinates :warn) (fnil inc 0)))
+     board
+     ws)))
+
+(defn init-game
+  [width height mine-count]
+  (-> (empty-board width height)
+      (place-mines mine-count)
+      (place-warnings)))
 
 
-(def glider (populate (empty-board 10 10) #{[1 1] [1 2]}))
-(pprint  glider)
+
