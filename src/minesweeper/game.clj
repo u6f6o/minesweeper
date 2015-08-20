bcf6e43b8f55f8a5038e841b0c84ca049917a2f1
Ulf Gitschthaler <ulf@gitschthaler.de>
2015-08-20 21:16:40 +0200

    add functions to check if mine triggered or board cleared

--- src/minesweeper/game.clj
+++ src/minesweeper/game.clj
@@ -1,10 +1,12 @@
 (ns minesweeper.game
   (:use [clojure.pprint]))
 
+
 (defn- empty-board
   "Create a rectangular empty board of the specified with and height"
   [w h]
   (vec (repeat w (vec (repeat h nil)))))
+
 
 (defn to-coords
   ([board]
@@ -15,25 +17,18 @@
      (for [x (range w) y (range h) :when (pred (get-in board [x y]))]
        [x y]))))
 
+
 (defn board->coords
   "Coordinates of the board"
   [board]
   (to-coords board))
+
 
 (defn mines->coords
   "Coordinates of the mines on the board"
   [board]
   (to-coords board :mine))
 
-(defn- place-mines
-  "Place n mines randomly on the board"
-  [board n]
-  (let [mines (take n (shuffle (to-coords board)))]
-    (reduce
-     (fn [board coordinates]
-       (assoc-in board coordinates {:mine true}))
-     board
-     mines)))
 
 (defn neighbours
   "Locate neighbour cells based on coordinates [x y],
@@ -48,7 +43,19 @@
                      (> h dy -1))]
       [dx dy])))
 
-(defn- place-warnings
+
+(defn place-mines
+  "Place n mines randomly on the board"
+  [board n]
+  (let [mines (take n (shuffle (to-coords board)))]
+    (reduce
+     (fn [board coordinates]
+       (assoc-in board coordinates {:mine true}))
+     board
+     mines)))
+
+
+(defn place-warnings
   "Place warnings on a mines' neighbour cells"
   [board]
   (let [ws (->> board
@@ -61,11 +68,28 @@
      board
      ws)))
 
+
 (defn init-game
+  "Create board and place mines and warnings"
   [width height mine-count]
   (-> (empty-board width height)
       (place-mines mine-count)
       (place-warnings)))
+
+
+(defn mine-triggered?
+  [board]
+  (letfn [(pred [m] (every? (or m {}) [:mine :flag]))]
+    (not-empty
+      (to-coords board pred))))
+
+
+(defn board-cleared?
+  [board]
+  (letfn [(pred [m] (not-any? (or m {}) [:mine :flag]))]
+    (empty?
+     (to-coords board pred))))
+
 
 
 
