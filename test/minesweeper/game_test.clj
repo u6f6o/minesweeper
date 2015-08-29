2f8e0cb8f85c0fb799369b01e9d77e530104e268
Ulf Gitschthaler <ulf@gitschthaler.de>
2015-08-29 21:10:43 +0200

    additional tests

--- test/minesweeper/game_test.clj
+++ test/minesweeper/game_test.clj
@@ -129,6 +129,30 @@
 
 
 
+(deftest test-warnings-freq
+  (testing "Multiple mines"
+    (let [board [[{}           {}           {}           {}           {}          ]
+                 [{:mine true} {:mine true} {}           {}           {}          ]
+                 [{}           {}           {}           {}           {}          ]
+                 [{}           {}           {:mine true} {:mine true} {:mine true}]
+                 [{}           {}           {:mine true} {}           {:mine true}]
+                 [{:mine true} {}           {:mine true} {:mine true} {:mine true}]]
+          exp   {[0 0] 2
+                 [0 1] 2
+                 [0 2] 1
+                 [1 2] 1
+                 [2 0] 2
+                 [2 1] 3
+                 [2 2] 3
+                 [2 3] 3
+                 [2 4] 2
+                 [3 1] 2
+                 [4 0] 1
+                 [4 1] 4
+                 [4 3] 8
+                 [5 1] 3}]
+      (is
+       (= exp (warnings-freq board))))))
 
 
 
