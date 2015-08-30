5b789304a276cca07a2b0292b682ad93d71a174c
Ulf Gitschthaler <ulf@gitschthaler.de>
2015-08-30 11:26:06 +0200

    change icons handling

---
+++ src/minesweeper/icons.clj
@@ -0,0 +1,41 @@
+(ns minesweeper.file
+  "Icons control utility"
+  (:require
+    [clojure.java.io :as io]
+    [clojure.string  :as str]
+    [seesaw.icon :as icon])
+  (:import
+    [java.io File]))
+
+
+(def ^:private cell-icons-path "minesweeper/icons/cell")
+(def ^:private face-icons-path "minesweeper/icons/face")
+
+
+(defn- file-name
+  [file]
+  (str/replace-first
+   (.getName file) #"\.[^.]+$" ""))
+
+
+(def ^:private init-icons
+  (memoize
+   (fn [res]
+     (let [parent (comp rest file-seq io/file io/resource res)]
+       (reduce
+        #(assoc %1 (keyword (file-name %2)) (icon/icon %2))
+        {}
+        parent)))))
+
+
+(defn cell-icon
+  [id]
+  (let [icons (init-icons cell-icons-path)]
+    (get icons id)))
+
+
+(defn face-icon
+  [id]
+  (let [icons (init-icons face-icons-path)]
+    (get icons id)))
+
