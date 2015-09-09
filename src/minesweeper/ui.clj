08ce652bab2c84ca573334a7db1ca5b03b9ca8d9
Ulf Gitschthaler <ulf@gitschthaler.de>
2015-09-09 13:52:31 +0200

    change difficulty level settings

--- src/minesweeper/ui.clj
+++ src/minesweeper/ui.clj
@@ -13,7 +13,7 @@
 
 (def levels { :beginner     { :rows 8,  :cols 8,  :mines 10 }
               :intermediate { :rows 16, :cols 16, :mines 40 }
-              :expert       { :rows 16, :cols 30, :mines 99 }})
+              :expert       { :rows 30, :cols 16, :mines 99 }})
 
 
 
