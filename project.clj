30e4e06175911ac864bdbbbf23397043e112c1d0
Ulf Gitschthaler <ulf@gitschthaler.de>
2015-10-10 00:21:01 +0200

    upgrade clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/u6f6o/minesweeper"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [seesaw "1.4.5"]]
   :main ^:skip-aot minesweeper.ui
   :target-path "target/%s"
