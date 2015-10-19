173a5ff354d36f16d8375f9b3e6f6739cd125ddf
Ulf Gitschthaler <ulf@gitschthaler.de>
2015-10-19 20:57:53 +0200

    try piotrs suggestion

--- src/minesweeper/board.clj
+++ src/minesweeper/board.clj
@@ -2,61 +2,93 @@
   (:use [clojure.pprint]))
 
 
+
+(defn index*
+  [row-size]
+  (fn [x y]
+    (+ x (* y row-size))))
+
+(defn coord*
+  [row-size]
+  (fn [idx]
+    [(mod idx row-size) (quot idx row-size)]))
+
+(defn size*
+  [row-size]
+  (fn [board]
+    [row-size (quot (count board) row-size)]))
+
+
+(def index-8 (index* 8))
+(def coord-8 (coord* 8))
+(def size-8 (size* 8))
+
+
+(defn idx->coords
+  [idx]
+  (coord-8 idx))
+
+(defn coords->idx
+  [x y]
+  (index-8 x y))
+
+(defn board-size
+  [board]
+  (size-8 board))
+
+
+;; TODO rename
+(defn filter-cells
+  [board pred]
+  (keep-indexed
+   (fn [i x] (when (pred x) i)) board))
+
+
 (defn empty-board
-  "Create a rectangular empty board of
-  the specified with and height"
+  "Create an empty board with
+  the specified width and height"
   [w h]
-  (vec (repeat w (vec (repeat h {})))))
-
-
-(defn to-coords
-  "Transform the board cells into coordinates"
-  ([board]
-   (to-coords board (constantly true)))
-  ([board pred]
-   (let [w (count board)
-         h (count (first board))]
-     (for [x (range w) y (range h) :when (pred (get-in board [x y]))]
-       [x y]))))
+  (vec (repeat (* w h) {})))
 
 
 (defn neighbour-cells
   "Locate neighbour cells based on coordinates [x y],
   respecting board width and height"
-  [board [x y]]
-  (let [w (count board)
-        h (count (first board))]
+  [board idx]
+  (let [[x y] (idx->coords idx)
+        [w h] (board-size board)]
     (for [dx (map (partial + x) [-1 0 1])
           dy (map (partial + y) [-1 0 1])
           :when (and (or (not= x dx) (not= y dy))
                      (> w dx -1)
                      (> h dy -1))]
-      [dx dy])))
+      (coords->idx dx dy))))
 
 
-(defn warnings-freq [board]
+(defn warnings-freq
+  [board]
   "Count the number of nearby mines"
-  (let [mines (to-coords board :mine)
+  (let [mines    (filter-cells board :mine)
         warnings (mapcat (partial neighbour-cells board) mines)]
     (frequencies
-     (remove (set mines) warnings))))
+      (remove (set mines) warnings))))
 
 
 (defn random-mines
-  [board start-pos]
-  (-> (set (to-coords board))
-      (disj start-pos)
-      (shuffle)))
+  [board mine-cnt start-pos]
+  (take mine-cnt
+        (-> (set (range (count board)))
+            (disj start-pos)
+            (shuffle))))
 
 
 (defn place-mines
   "Place n mines randomly on the board"
   [board mine-count start-pos]
-  (let [mines (take mine-count
-                    (random-mines board start-pos))]
+  (let [mines (random-mines board mine-count start-pos)]
     (reduce
-     (fn [m k]
-       (assoc-in m k {:mine true}))
+     (fn [v i]
+       (update v i conj {:mine true}))
      board
      mines)))
 
@@ -66,8 +98,8 @@
   [board]
   (let [mine-counts (warnings-freq board)]
     (reduce-kv
-     (fn [m k v]
-       (assoc-in m k {:warn v}))
+     (fn [v i c]
+       (update v i conj {:warn c}))
      board
      mine-counts)))
 
@@ -75,32 +107,34 @@
 (defn explore-field
   "Explore single field on the board"
   [board coords]
-  (update-in board coords conj {:explored true}))
+  (let [idx (coords->idx coords)]
+    (update board idx conj {:explored true})))
 
 
 (defn toggle-flag
   "Sets and removes a flag"
   [board coords]
-  (update-in board coords
-             #(assoc % :flag (not (:flag %)))))
+  (let [idx (coords->idx coords)]
+    (update board
+            idx
+            #(assoc % :flag (not (:flag %))))))
 
 
 (defn game-started?
   "At least one field explored?"
   [board]
-  (pos? (count (to-coords board :explored))))
+  (some (partial :explored) board))
 
 
 (defn game-lost?
   "Any mine exploded?"
   [board]
   (letfn [(pred [m] (and (:mine m) (:explored m)))]
-    (pos? (count (to-coords board pred)))))
+    (some (partial pred) board)))
 
 
 (defn game-won?
   "All fields cleared?"
   [board]
   (letfn [(pred [m] (or (:mine m) (:explored m)))]
-    (= (to-coords board pred)
-       (to-coords board))))
+    (every? (partial pred) board)))
